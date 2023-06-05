cfced221db884bea50b841fbba91d558287051ee
unknown <talyshao@gmail.com>
2023-06-05 13:42:17 -0500

    added Order Page

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class OrderPage {
+	
+	//assume its V machine
+	
+	By loc = By.id("order");
+	
+	public void getOrder() {
+		System.out.println("get order");
+	}
+
+}
