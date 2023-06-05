ef770d9ef0fc0ec5a4852b7c3b81fcf2ca6cba79
unknown <talyshao@gmail.com>
2023-06-05 13:59:54 -0500

    updated as per Safina comments

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -7,9 +7,13 @@ public class OrderPage {
 	//assume its V machine
 	
 	By loc = By.id("order");
+	By email = By.id("email");
 	
 	public void getOrder() {
 		System.out.println("get order");
 	}
 
+	public void getEmail() {
+		System.out.println("get email id");
+	}
 }
