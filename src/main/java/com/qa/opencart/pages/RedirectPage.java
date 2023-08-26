391b121994c4552f038080fdbd31f677bda56a79
safinashao <talyshao@gmail.com>
2023-08-26 10:45:26 -0500

    added a new page class RedirectPage

---
+++ src/main/java/com/qa/opencart/pages/RedirectPage.java
@@ -0,0 +1,18 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class RedirectPage {
+
+	private WebDriver driver;
+	
+	public RedirectPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	//create By locators
+	
+	
+	//add page actions
+	
+}
