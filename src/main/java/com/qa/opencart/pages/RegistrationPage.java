5ac37862f18b91c4a2f38c82c786bcb5593ca6cb
safinashao <talyshao@gmail.com>
2023-08-26 13:39:28 -0500

    added by locators

--- src/main/java/com/qa/opencart/pages/RegistrationPage.java
+++ src/main/java/com/qa/opencart/pages/RegistrationPage.java
@@ -1,5 +1,6 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class RegistrationPage {
@@ -7,8 +8,12 @@ public class RegistrationPage {
 	private WebDriver driver;
 	
 	public RegistrationPage() {
-		this.driver = driver;
-		
+		this.driver = driver;	
 	}
+	
+	By firstName = By.id("first_name");
+	By lastName = By.name("last_name");
+	
+	
 
 }
